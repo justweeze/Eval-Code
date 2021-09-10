3c7644d6ea71a8d4f7c650175865d4ec1f0fafc7
Call me...leo <74614943+its-leo-bitch@users.noreply.github.com>
2021-09-10 11:31:03 +0530

    Client

---
+++ piston/client.py
@@ -0,0 +1,60 @@
+# © its-leo-bitch
+from typing import Union, Optional
+from urllib.parse import quote
+
+import aiohttp
+import types
+
+from .utils import Attrify as attr
+from .exceptions import *
+from .types import Language
+
+class Piston:
+    def __init__(
+        self,
+        base_url: Optional[str] = "https://emkc.org/api/v2/piston"
+    ):
+        """Setup all urls and session."""
+        self.base_url = base_url
+
+    async def execute(
+        self,
+        language=str,
+        source=str,
+        version=str,
+        stdin: str = None,
+        args: list = None
+    ) -> attr:
+        url = f"{self.base_url}/execute"
+        data = {
+            'language': language,
+            'version': version,
+            'files': source
+        }
+        if stdin:
+            data['stdin'] = stdin
+        if args:
+            data['args'] = args
+        async with aiohttp.ClientSession() as ses:
+            async with ses.post(url, data=data) as resp:
+                try:
+                    return attr((await resp.json()))
+                except aiohttp.client_exceptions.ContentTypeError:
+                    raise EndpointDown("Client cannot reach the Endpoint")
+            await ses.close()
+
+    async def versions(self) -> None:
+        url = f"{self.base_url}/runtimes"
+        async with aiohttp.ClientSession() as ses:
+            async with ses.get(url) as resp:
+                out = await resp.json()
+                vers = []
+                try:
+                    for l in out:
+                        vers.append(Language(l))
+                    return (vers)
+                except aiohttp.client_exceptions.ContentTypeError:
+                    raise EndpointDown("Client cannot reach the Endpoint")
+            await ses.close()
+        
+        
